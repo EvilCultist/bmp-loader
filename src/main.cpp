87d6413fa039b0b3881475f6350d3a3ed9829779
me <66961093+EvilCultist@users.noreply.github.com>
2025-01-24 14:27:11 +0530

    started file reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 int main() {
+<<<<<<< HEAD
   std::string file = "images/mask.bmp";
+=======
+  std::string filePath = "images/mask.bmp";
+  std::ifstream file(filePath, std::ios::binary);
+  std::uint8_t *fileHeader = nullptr;
+  std::uint8_t *dibHeader = nullptr;
+  char databuff[] = {'\0', '\0'};
+  std::uint8_t *pixels = nullptr;
+  char fileSize[4]; // = {0x00, 0x00, 0x00, 0x00};
+
+  if (!file) {
+    std::cerr << "file not found" << std::endl;
+    return -1;
+  }
+  file.read(databuff, sizeof(databuff));
+  if (!(databuff[0] == 'B' && databuff[1] == 'M')) {
+    std::cerr << "file not bmp" << std::endl;
+  }
+  file.read(fileSize, sizeof(fileSize));
+  for (auto val : fileSize) {
+    std::cout << (uint16_t)val << " ";
+  }
+  std::cout << std::endl;
+>>>>>>> d7cd858 (started file reading)
   std::cout << "hello world" << std::endl;
 }
